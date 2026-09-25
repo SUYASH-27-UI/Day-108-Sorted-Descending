4f3c390da3e714be22523cc07e38aa87e461bd3a
SUYASH-27-UI <suyashsurve30@gmail.com>
2026-09-25 13:18:41 +0530

    Create sorted_descending.py

---
+++ sorted_descending.py
@@ -0,0 +1,8 @@
+numbers = [20, 50, 10, 40, 30]
+
+print("Original list:", numbers)
+
+sorted_numbers = sorted(numbers, reverse=True)
+
+print("Descending list:", sorted_numbers)
+print("Original list:", numbers)
